cb7ea99fb5db3b138247ac7f2df872ab2e2d2f11
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-29 22:12:42 +0900

    feat: layout chat view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
+import { ChatInput } from './_components/ChatInput';
+import { ChatView } from './_components/ChatView';
 import { BaseLayout } from '@/components/layouts/BaseLayout';
 
 export default function Home() {
   return (
     <BaseLayout>
-      <div className="flex h-[calc(100vh-100px)]">
-        <div className="w-[60%] bg-gray-300"></div>
-        <div className="w-[40%] bg-gray-500"></div>
+      <div className="flex divide-x divide-gray-200">
+        <div className="flex-[3_3_0%]"></div>
+        <div className="h-[calc(100vh-100px)] flex-[2_2_0%] overflow-scroll bg-slate-50 px-4 pb-32 pt-4">
+          <ChatView />
+        </div>
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 z-50 border-t border-gray-200 bg-slate-50 px-4 py-8">
+        <ChatInput />
       </div>
     </BaseLayout>
   );
