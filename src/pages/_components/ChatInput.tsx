c30a994e9b01d8dbc5aa1faada588e40321ceebd
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-29 22:11:01 +0900

    feat: add ChatInput

---
+++ src/pages/_components/ChatInput.tsx
@@ -0,0 +1,17 @@
+import { Textarea } from '@mantine/core';
+import { Button } from '@mantine/core';
+
+export const ChatInput = () => {
+  return (
+    <div className="flex items-end gap-x-2">
+      <Textarea
+        placeholder="やったことや今の気分を入力してください"
+        autosize
+        minRows={1}
+        maxRows={4}
+        className="flex-1"
+      />
+      <Button color="pink">送信</Button>
+    </div>
+  );
+};
