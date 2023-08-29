1cc50675569dc72b9732e1b860ec135da3a258f7
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-29 17:07:59 +0900

    feat: add login page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-white text-slate-700">
         <Main />
         <NextScript />
       </body>
---
+++ src/pages/login.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from 'next';
+
+import { Button } from 'flowbite-react';
+import Image from 'next/image';
+
+const LoginPage: NextPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-b from-white via-blue-100 to-blue-400 px-4">
+      <div>
+        <div className="flex items-center justify-between gap-8">
+          <div>
+            <div className="space-y-8">
+              <h1 className="bg-gradient-to-r from-pink-400 to-pink-900 bg-clip-text text-6xl font-bold text-transparent">
+                Tarako
+              </h1>
+              <div className="space-y-1 text-4xl font-bold">
+                <p>個人作業が多い組織のための</p>
+                <p>情報可視化ツール</p>
+              </div>
+              <div className="space-y-1 text-xl text-slate-500">
+                <p>
+                  個人作業が多い組織は、誰が何をしているのかが見えにくいです。
+                </p>
+                <p>
+                  Tarakoは個人の作業を可視化することで、組織の情報を可視化します。
+                </p>
+              </div>
+            </div>
+          </div>
+          <Image
+            src="/logo.png"
+            alt="Tarako"
+            width={250}
+            height={250}
+            className="flex-shrink-0"
+          />
+        </div>
+        <div className="mt-12">
+          <p className="text-2xl font-bold">アプリを体験する</p>
+          <div className="mt-4 flex gap-x-4 text-white">
+            <Button size="xl">事務員1</Button>
+            <Button size="xl">事務員2</Button>
+            <Button size="xl">上司</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
