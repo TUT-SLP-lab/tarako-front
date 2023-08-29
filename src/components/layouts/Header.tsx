10c3d6f8961596247bd2920982991536b84ebbed
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-29 21:58:43 +0900

    chore: modify header layout

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className="fixed left-0 right-0 top-0 z-50 flex h-header items-center justify-between bg-white px-8 shadow-md">
+    <header className="fixed left-0 right-0 top-0 z-50 flex h-header items-center justify-between border-b border-gray-200 bg-white px-8">
       <Link href="/">
         <Image
           src="/logo.png"
