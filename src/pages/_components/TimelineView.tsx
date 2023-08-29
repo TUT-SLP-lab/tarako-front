d6508b4107075b45e5ab252fa1df041d0c39213b
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-29 23:27:31 +0900

    feat: add TODO comment

--- src/pages/_components/TimelineView.tsx
+++ src/pages/_components/TimelineView.tsx
@@ -1,6 +1,7 @@
 import { Timeline, Text } from '@mantine/core';
 import { IconGitBranch } from '@tabler/icons-react';
 
+/** TODO: めちゃくちゃ雑に実装しているので、レイアウト修正しっかりやる */
 export const TimelineView = () => {
   return (
     <Timeline bulletSize={24}>
