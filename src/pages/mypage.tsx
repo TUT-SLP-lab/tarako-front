e808a3f764067d44bf227cdca0f6fa18f3d7abe9
Nagito <uextyou@gmail.com>
2023-08-30 20:15:49 +0900

    [ADD] Page for checking components

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next';
 
+import IncompleteTask from '@/components/common/IncompleteTask';
+import SeverityTransition from '@/components/common/SeverityTransition';
+import TodaysSeverity from '@/components/common/TodaysSeverity';
+import { Grid } from '@mantine/core';
+
 const MyPage: NextPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <>
+      <Grid>
+        <Grid.Col span={4}>
+          <IncompleteTask />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <TodaysSeverity />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <SeverityTransition />
+        </Grid.Col>
+      </Grid>
+    </>
+  );
 };
 
 export default MyPage;
