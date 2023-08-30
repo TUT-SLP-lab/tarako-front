946916247274546d6f185557b94676dd9561e964
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-30 13:30:17 +0900

    feat: view max-width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,18 +11,20 @@ export default function Home() {
     <BaseAuthLayout>
       <div className="flex divide-x divide-gray-200">
         <div className="h-[calc(100vh-100px)] flex-[3_3_0%] overflow-auto px-8 pb-16 pt-8">
-          <UserStatusList />
-          <section className="mt-12">
-            <div className="flex items-center justify-between">
-              <h2 className="text-3xl font-bold">Today</h2>
-              <Button size="lg" variant="outline" color="pink">
-                日報を作成する
-              </Button>
-            </div>
-            <div className="mt-8">
-              <TimelineView />
-            </div>
-          </section>
+          <div className="mx-auto max-w-screen-lg">
+            <UserStatusList />
+            <section className="mt-12">
+              <div className="flex items-center justify-between">
+                <h2 className="text-3xl font-bold">Today</h2>
+                <Button size="lg" variant="outline" color="pink">
+                  日報を作成する
+                </Button>
+              </div>
+              <div className="mt-8">
+                <TimelineView />
+              </div>
+            </section>
+          </div>
         </div>
         <div className="h-[calc(100vh-100px)] flex-[2_2_0%] bg-slate-50">
           <div className="flex h-full flex-col divide-y divide-gray-200">
