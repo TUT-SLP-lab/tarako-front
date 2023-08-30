5552d44a2fd8472d69f29cdcdfa9a255179da3ef
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-30 14:54:59 +0900

    feat: add section page

---
+++ src/pages/section/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+import { BaseAuthLayout } from '@/components/layouts/BaseAuthLayout';
+
+const SectionPage: NextPage = () => {
+  return (
+    <BaseAuthLayout>
+      <h1>SectionPage</h1>
+    </BaseAuthLayout>
+  );
+};
+
+export default SectionPage;
