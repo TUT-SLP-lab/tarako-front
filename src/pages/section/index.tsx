4d1d5913c446e23b6bdab1eb38403ec004ea88dd
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-30 17:32:45 +0900

    feat: add section page skelton

--- src/pages/section/index.tsx
+++ src/pages/section/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next';
 
+import { PageTitle } from '@/components/common/PageTitle';
 import { BaseAuthLayout } from '@/components/layouts/BaseAuthLayout';
 
 const SectionPage: NextPage = () => {
   return (
     <BaseAuthLayout>
-      <h1>SectionPage</h1>
+      <div className="mx-auto max-w-screen-lg p-8">
+        <PageTitle>部署ページ</PageTitle>
+        <div className="flex h-[280px] animate-pulse gap-4">
+          <div className="flex-1 rounded-lg bg-gray-200" />
+          <div className="flex-1 rounded-lg  bg-gray-200" />
+          <div className="flex-1 rounded-lg bg-gray-200" />
+        </div>
+      </div>
     </BaseAuthLayout>
   );
 };
