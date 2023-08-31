c64f1cbce7224fe33f9c4a4ca797c5e6c4d4178e
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-08-31 19:38:11 +0900

    feat: fix routing settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/pageExtensions#including-non-page-files-in-the-pages-directory
+   */
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 };
 
 module.exports = nextConfig;
