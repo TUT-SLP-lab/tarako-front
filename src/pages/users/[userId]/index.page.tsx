08741dead57f9b869d8a7f94e55920987f988ec0
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-09-01 13:53:50 +0900

    feat: add head title

--- src/pages/users/[userId]/index.page.tsx
+++ src/pages/users/[userId]/index.page.tsx
@@ -4,6 +4,7 @@ import { TaskTimeline } from '@/components/features/task/TaskTimeline';
 import { BaseAuthLayout } from '@/components/layouts/BaseAuthLayout';
 import { mockUsers } from '@/mocks/mockUsers';
 import { Avatar } from '@mantine/core';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const UserDetailPage: NextPage = () => {
@@ -21,20 +22,25 @@ const UserDetailPage: NextPage = () => {
   }
 
   return (
-    <BaseAuthLayout>
-      <div className="mx-auto max-w-screen-lg space-y-12 p-8">
-        <section className="flex items-center justify-center gap-16">
-          <Avatar src={user.icon} size={320} />
-          <div>
-            <h1 className="text-3xl font-bold">{user.name}</h1>
-            <p className="mt-1 text-lg">{user.section}</p>
-            <p className="text-lg">{user.email}</p>
-            <p className="mt-4 text-lg">{user.description}</p>
-          </div>
-        </section>
-        <TaskTimeline />
-      </div>
-    </BaseAuthLayout>
+    <>
+      <Head>
+        <title>{user.name}のプロフィール - Tarako</title>
+      </Head>
+      <BaseAuthLayout>
+        <div className="mx-auto max-w-screen-lg space-y-12 p-8">
+          <section className="flex items-center justify-center gap-16">
+            <Avatar src={user.icon} size={320} />
+            <div>
+              <h1 className="text-3xl font-bold">{user.name}</h1>
+              <p className="mt-1 text-lg">{user.section}</p>
+              <p className="text-lg">{user.email}</p>
+              <p className="mt-4 text-lg">{user.description}</p>
+            </div>
+          </section>
+          <TaskTimeline />
+        </div>
+      </BaseAuthLayout>
+    </>
   );
 };
 
